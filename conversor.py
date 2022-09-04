990c01e26adc46a6d3cc1778d8a11ffabe475185
Yuliethmguevara <yumguevarala@unal.edu.co>
2022-09-04 13:14:38 -0500

    Esta herramienta es un conversor de moneda

---
+++ conversor.py
@@ -0,0 +1,34 @@
+from tokenize import PseudoExtras
+
+def conversor (tipo_pesos, valor_dolar):
+    pesos = input("¿Cuántos pesos " + tipo_pesos + " tiene?: ")
+    pesos = float(pesos)
+    dolares= pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares= str(dolares)
+    print("valor total: "+ dolares + " dólares")
+
+
+menu = """
+Bienvenido al conversor de monedas 💸💸
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input (menu))
+
+if opcion == 1 :
+    conversor("colombianos", 3875)
+elif opcion ==2 : 
+    conversor("argentinos", 65)
+elif opcion == 3 :
+    conversor("mexicanos", 24)
+else :
+    print ("Ingresa una opción correcta por favor")
+    
+
+
+

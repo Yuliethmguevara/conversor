51e9f52a628ea0fb0c5df8a6af766b895c78cb54
Yuliethmguevara <yumguevarala@unal.edu.co>
2022-09-08 17:40:58 -0500

    Prueba

---
+++ pru.py
@@ -0,0 +1,25 @@
+import random
+
+# def run():
+#     comidas = ("Changua", "Huevos", "Cereal", "Calentado", "ni mierd..", "maiz pira")
+#     signos = ("Geminis", "Escorpio", "Tauro", "Aries", "Acuario", "Leo", "Libra")
+#     opcion = random.choice(comidas)
+#     opcion1 = random.choice(signos)
+
+#     print("Hoy desayunarás " + opcion + " con un: " + opcion1)
+
+
+
+
+# if __name__ == "__main__":
+#     run()
+
+
+def run():
+    comida = ("ajiaco", "lechona")
+    opcion = random.choice(comida)
+
+    print("tu cena será " + opcion)
+
+if __name__ == "__main__":
+     run()
